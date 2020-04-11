376d801acdc5ce8f9c3a348cb1eb2da0f6159c02
grestonian <gsingh0719@gmail.com>
2020-04-11 18:51:38 +0530

    W4-npm scripts part1

---
+++ 1-Front-End Web UI Frameworks and Tools: Bootstrap 4/WEEK1/Bootstrap4/conFusion/js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#myCarousel').carousel({interval:2000});
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#myCarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#myCarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#reserveTableModalBtn').click(function() {
+        $('#reserveTableModal').modal('toggle');
+    });
+    $('#loginModalBtn').click(function() {
+        $('#loginModal').modal('toggle');
+    });
+});
