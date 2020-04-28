f1e49518fbf7f84b77c2cff92ca6bde1f9586380
grestonian <gsingh0719@gmail.com>
2020-04-28 23:19:25 +0530

    2-W1-Components Part 2

--- 2-Front-End Web Development with React/confusion/src/App.js
+++ 2-Front-End Web Development with React/confusion/src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes'
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES      // here we have uplifted the DISHES object variable to App.js, now we can make this available to any component(here for example 'Menu' component)
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
