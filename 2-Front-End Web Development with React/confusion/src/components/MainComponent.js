5942b22e1d9e8dced64e655db08ca6f5a588f38c
grestonian <gsingh0719@gmail.com>
2020-05-05 19:42:07 +0530

    2-W2-Header & Footer

--- 2-Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ 2-Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -1,9 +1,10 @@
 // This component will be acting as a sudo containder component
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent'
-import DishDetail from './DishdetailComponent'
-import { DISHES } from '../shared/dishes'
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import { DISHES } from '../shared/dishes';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -22,16 +23,12 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header/>
         <div className="container">
           <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]}/>
         </div>
-        
+        <Footer/>
       </div>
     );
   }
