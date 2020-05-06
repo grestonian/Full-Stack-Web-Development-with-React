1e6daf4df5411630f0f2e78af2d3fdbbf66337da
grestonian <gsingh0719@gmail.com>
2020-05-06 13:33:35 +0530

    2-W2-Single Page Application Part1

--- 2-Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ 2-Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -2,9 +2,12 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
@@ -14,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,      // here we have uplifted the DISHES object variable to MainComponent.js, now we can make this available to any component(here for example 'Menu' component)
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
     };
   }
 
@@ -21,7 +27,9 @@ class Main extends Component {
 
     const Homepage = () => {
       return(
-         <Home/>
+         <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
       )
     }
     return (
@@ -30,6 +38,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={Homepage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to='/home' />
         </Switch>
         <Footer/>
