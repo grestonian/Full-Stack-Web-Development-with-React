51ba9a302ffa3e7b0ec63786c78af2bde505a0c7
grestonian <gsingh0719@gmail.com>
2021-01-28 12:12:35 +0530

    2-W2-Header and Footer

--- 2-Front-End Development with React/confusion/src/Components/MainComponent.js
+++ 2-Front-End Development with React/confusion/src/Components/MainComponent.js
@@ -1,8 +1,9 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes.js';
 import { Component } from 'react';
 import DishDetail from './DishDetailComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component{
 
@@ -22,15 +23,12 @@ class Main extends Component{
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
+          <Header/>
           <Menu dishes={this.state.dishes}
             onClick={(dishId) => this.onDishSelect(dishId)}/>
           <DishDetail 
             dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+          <Footer/>
       </div>
     );
   }
